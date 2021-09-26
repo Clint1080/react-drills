870efac36afbcbace3c3714bb1d7719344a8a4ed
Clint Edwards <cedwards1080@gmail.com>
2021-09-26 16:10:00 -0600

    finished app 5 and install npm in all apps

---
+++ app-5/src/Components/Image.js
@@ -0,0 +1,12 @@
+import React from "react";
+import matrix from "../Assets/matrix.jpg";
+
+const Image = () => {
+  return (
+    <div>
+      <img src={matrix}></img>
+    </div>
+  );
+};
+
+export default Image;
